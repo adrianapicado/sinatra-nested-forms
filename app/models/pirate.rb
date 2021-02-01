d39129b685fed9c610d64a574631d851e714134d
adriana <picadoadriana7@gmail.com>
2021-02-01 10:54:37 -0500

    commit tree 2798dbb56a8a190441e5f17a93db8d2d47b27839

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize 
+      @name = args[:name]
+      @weight = args[:weight]
+      @height = args[:height]
+
+      PIRATES << self 
+
+    end
+
 end
